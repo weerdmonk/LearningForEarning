f2aa194c31a0ad4e4d8ba9bb7bfdf8b615250df5
Jyotiprakash Sahoo <jp.sahoo1991@gmail.com>
2017-05-21 11:49:27 +0530

    shuntYard: created circular queue.

--- shuntYard/main.cpp
+++ shuntYard/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 typedef struct _stack
 {
     void **array;
-    int capacity, top;
+    unsigned int capacity, top;
 
     _stack(int size)
     {
@@ -26,23 +26,94 @@ typedef struct _stack
 
     void push(void *data)
     {
-        if ( !isFull() )
-            array[top++] = data;
-        else
-            cout << "Stack is full!\n";
+      if (data == NULL)
+      {
+        return;
+      }
+      if ( isFull() != true )
+      {
+        array[top++] = data;
+      }
+      else
+      {
+        cout << "Stack is full!\n";
+      }
     }
 
     void *pop()
     {
-        void *data = NULL;
-        if ( !isEmpty() )
-            data = array[--top];
-        else
-            cout << "Stack is empty!\n";
+      void *data = NULL;
+      if ( isEmpty() != true )
+      {
+        data = array[--top];
+      }
+      else
+      {
+        cout << "Stack is empty!\n";
+      }
 
-        return data;
+      return data;
     }
 } Stack;
+
+typedef struct _queue
+{
+  void **array;
+  unsigned int capacity, front, rear, size;
+
+  _queue(int _capacity)
+  {
+    capacity = _capacity;
+    array = new void*[capacity];
+    front = size = 0;
+    rear = capacity - 1;
+  }
+  
+  ~_queue()
+  {
+    delete[] array;
+  }
+
+  bool isEmpty() { return size == 0; }
+
+  bool isFull() { return size == capacity; }
+
+  void enqueue(void *data)
+  {
+    if (data == NULL)
+    {
+      return;
+    }
+
+    if ( isFull() == true )
+    {
+      cout << "Queue is full!\n";
+      return;
+    }
+
+    rear = ++rear % capacity;
+    array[rear] = data;
+    ++size;
+  }
+
+  void *dequeue()
+  {
+    void *data = NULL;
+
+    if ( isEmpty() == true ) 
+    {
+      cout << "Queue is empty!\n";
+      return data;
+    }
+
+    data = array[front];
+    front = ++front % capacity;
+    --size;
+
+    return data;
+  }
+
+} Queue;
 
 int main()
 
@@ -53,6 +124,14 @@ int main()
     *d2 = 20;
     int *d3 = new int;
     *d3 = 30;
+    int *d4 = new int;
+    *d4 = 40;
+    int *d5 = new int;
+    *d5 = 50;
+    int *d6 = new int;
+    *d6 = 60;
+
+    int *data;
 
     Stack s(3);
 
@@ -60,8 +139,6 @@ int main()
     s.push(d2);
     s.push(d3);
 
-    int *data;
-
     data = (int*)s.pop();
     if (data) cout << *data << "\n";
     data = (int*)s.pop();
@@ -70,7 +147,44 @@ int main()
     if (data) cout << *data << "\n";
     data = (int*)s.pop();
 
-    delete d1, d2, d3;
+    cout << "**************\n";
+
+    Queue q(3);
+
+    q.enqueue(d1);
+    q.enqueue(d2);
+    q.enqueue(d3);
+    q.enqueue(d4);
+    
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+    data = (int*)q.dequeue();
+
+    q.enqueue(d4);
+    q.enqueue(d5);
+    q.enqueue(d6);
+
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+
+    q.enqueue(d1);
+    q.enqueue(d2);
+
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+    data = (int*)q.dequeue();
+    if (data) cout << *data << "\n";
+    data = (int*)q.dequeue();
+
+    cout << "**************\n";
+
+    delete d1, d2, d3, d4, d5, d6;
 
 	return 0;
 }
